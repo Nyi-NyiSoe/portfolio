96ac156c98f39027db84a34d14c0cb98011b97dc
Nyi Nyi Soe <nyinyisoe.dev@gmail.com>
2025-02-21 16:04:59 +0700

    fix/fixed exp card height

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -174,7 +174,7 @@ const Skills = () => {
 
 const Experience = () => {
   return (
-    <div className="group bg-[#19191A] p-10 rounded-2xl shadow-lg flex flex-col items-start justify-center w-full relative overflow-hidden transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:shadow-blue-500/20 before:absolute before:w-full before:h-full before:-left-40 before:-top-40 before:bg-blue-500 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-all before:duration-500 before:scale-0 before:hover:opacity-10 before:hover:scale-100 after:absolute after:w-full after:h-full after:-right-40 after:-bottom-40 after:bg-purple-500 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-all after:duration-500 after:scale-0 after:hover:opacity-10 after:hover:scale-100">
+    <div className="group bg-[#19191A] p-10 rounded-2xl shadow-lg flex flex-col items-start justify-center w-full min-h-[310px] relative overflow-hidden transition-all duration-500 hover:scale-105 hover:shadow-2xl hover:shadow-blue-500/20 before:absolute before:w-full before:h-full before:-left-40 before:-top-40 before:bg-blue-500 before:rounded-full before:opacity-0 before:pointer-events-none before:transition-all before:duration-500 before:scale-0 before:hover:opacity-10 before:hover:scale-100 after:absolute after:w-full after:h-full after:-right-40 after:-bottom-40 after:bg-purple-500 after:rounded-full after:opacity-0 after:pointer-events-none after:transition-all after:duration-500 after:scale-0 after:hover:opacity-10 after:hover:scale-100">
       <div className="relative z-10 w-full">
         <h1 className="font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-400 mb-6">
           Experience
@@ -201,6 +201,7 @@ const Experience = () => {
     </div>
   );
 };
+
 
 const Education = () => {
   return (
